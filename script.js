9dcae95cb6fe1d0024f3e01b0eb2c4f7c63d26a4
Marlee Garcia <marlee.garcia121@gmail.com>
2022-05-02 13:45:31 -0400

    Area of circle function added

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ let radius = 4;
 let squareSide = 5;
 let triangleBase = 3;
 let triangleHeight = 6;
+
+function getAreaOfCircle(radius) {
+    let area = Math.pow(radius, 2) * Math.PI;
+    return area;
+}
+
+
+
+console.log(getAreaOfCircle(radius));
